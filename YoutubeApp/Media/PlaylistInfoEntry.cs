76ea43046e6bf1cd1f3b24184203737c77a4d8fd
legend2k <nice.sn2000@gmail.com>
2023-07-13 03:16:36 +0330

    Make the timestamp property of PlaylistInfoEntry nullable

--- YoutubeApp/Media/PlaylistInfoEntry.cs
+++ YoutubeApp/Media/PlaylistInfoEntry.cs
@@ -7,6 +7,6 @@ public class PlaylistInfoEntry
     public required string id { get; set; }
     public required string title { get; set; }
     public required int? duration { get; set; }
-    public required long timestamp { get; set; }
+    public required long? timestamp { get; set; }
     public required PlaylistInfoEntryThumbnail[] thumbnails { get; set; }
 }
