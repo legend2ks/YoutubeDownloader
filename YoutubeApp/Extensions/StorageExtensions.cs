5a446cd33e8320866f16b20a17646917da4b601c
legend2k <nice.sn2000@gmail.com>
2023-08-31 16:14:26 +0330

    Add StorageExtensions

---
+++ YoutubeApp/Extensions/StorageExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace YoutubeApp.Extensions;
+
+public static class StorageExtensions
+{
+    public static string GetActualPath(this DirectoryInfo directoryInfo)
+    {
+        if (directoryInfo.LinkTarget is not null)
+        {
+            return directoryInfo.LinkTarget;
+        }
+
+        var pathSegments = new List<string> { directoryInfo.Name };
+        var dirInfo = directoryInfo.Parent;
+
+        while (dirInfo is not null)
+        {
+            if (dirInfo.LinkTarget is not null)
+            {
+                pathSegments.Add(dirInfo.LinkTarget);
+                var target = Path.Join(pathSegments.AsEnumerable().Reverse().ToArray());
+                return target;
+            }
+
+            pathSegments.Add(dirInfo.Name);
+            dirInfo = dirInfo.Parent;
+        }
+
+        return directoryInfo.FullName;
+    }
+}
